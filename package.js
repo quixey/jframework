5c79610e2f8bcc9e750516219835f797e20a4887
Liron Shapira <liron@quixey.com>
2015-05-13 15:52:35 -0700

    Bump to 1.0.94 minor improvements

--- package.js
+++ package.js
@@ -9,7 +9,7 @@
 Package.describe({
     summary: "JFramework for Meteor + React",
     name: "quixey:jframework",
-    version: "1.0.93",
+    version: "1.0.94",
     git: "https://github.com/quixey/jframework.git"
 });
 
