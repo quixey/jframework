bb3b1264caa3e60d697d59fe3d223c7b0fd907e9
Marshall Quander <marshall@quixey.com>
2015-06-24 16:19:39 -0700

    Bump framework version

--- package.js
+++ package.js
@@ -9,7 +9,7 @@
 Package.describe({
     summary: "JFramework for Meteor + React",
     name: "quixey:jframework",
-    version: "1.3.7",
+    version: "1.3.8",
     git: "https://github.com/quixey/jframework.git"
 });
 
