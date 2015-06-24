b0c178e9017f1d18c7df1251b151a2eb9d36dbe8
Liron Shapira <liron@quixey.com>
2015-06-24 10:47:07 -0700

    1.3.7 minor fixes

--- package.js
+++ package.js
@@ -9,7 +9,7 @@
 Package.describe({
     summary: "JFramework for Meteor + React",
     name: "quixey:jframework",
-    version: "1.3.6",
+    version: "1.3.7",
     git: "https://github.com/quixey/jframework.git"
 });
 
