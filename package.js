eb0f101b350c49742cfdaa51c69998238bfa270f
Liron Shapira <liron@quixey.com>
2015-07-30 18:59:56 -0700

    1.5.6 - use worker for denorm queue

--- package.js
+++ package.js
@@ -9,7 +9,7 @@
 Package.describe({
     summary: "JFramework for Meteor + React",
     name: "quixey:jframework",
-    version: "1.5.5",
+    version: "1.5.6",
     git: "https://github.com/quixey/jframework.git"
 });
 
